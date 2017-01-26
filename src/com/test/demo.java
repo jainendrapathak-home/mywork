1e996d1d67e55cfe0a4d0d528a964f4e0fab2f0e
jainendra.pathak.home <jainendra.pathak@gmail.com>
2017-01-26 10:34:01 +0530

    JaxB Example

---
+++ src/com/test/demo.java
@@ -0,0 +1,47 @@
+package com.test;
+
+import java.io.FileInputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.util.StreamReaderDelegate;
+ 
+public class demo {
+ 
+    public static void main(String[] args) throws Exception {
+        JAXBContext jc = JAXBContext.newInstance(Customer.class);
+ 
+        XMLInputFactory xif = XMLInputFactory.newInstance();
+        XMLStreamReader xsr = xif.createXMLStreamReader(new FileInputStream("C:\\PersonalData\\Documents\\jai\\Myworkspace\\JaxBExample2\\src\\com\\test\\input.xml"));
+        xsr = new MyStreamReaderDelegate(xsr);
+ 
+        Unmarshaller unmarshaller = jc.createUnmarshaller();
+        Customer customer = (Customer) unmarshaller.unmarshal(xsr);
+ 
+        Marshaller marshaller = jc.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshaller.marshal(customer, System.out);
+    }
+ 
+    private static class MyStreamReaderDelegate extends StreamReaderDelegate {
+ 
+        public MyStreamReaderDelegate(XMLStreamReader xsr) {
+            super(xsr);
+        }
+ 
+        @Override
+        public String getAttributeLocalName(int index) {
+            return super.getAttributeLocalName(index).toLowerCase();
+        }
+ 
+        @Override
+        public String getLocalName() {
+            return super.getLocalName().toLowerCase();
+        }
+ 
+    }
+ 
+}
